e2e49408b1eba46ef1690b44293bf3b8e3b0a262
ylee <gabrieloboe@gmail.com>
2020-09-17 12:12:40 +0900

    2020 0910A반

--- 03http/src/main/java/com/ylee/a03http/MainActivity.java
+++ 03http/src/main/java/com/ylee/a03http/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -18,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button;
     TextView textView;
-    String naverurl;
-    String htmldata;
+    String naverurl="https://m.naver.com";
+    String htmldata = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                     BufferedReader reader = new BufferedReader
                             (new InputStreamReader(conn.getInputStream()));
                     String line;
-                    htmldata = null;
+                    htmldata = "RR";
                     while (true){
                         line = reader.readLine();
                         if(line == null)
