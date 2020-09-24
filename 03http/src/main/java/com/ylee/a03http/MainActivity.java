a5bc39268f95f849b1cbdfbeb2bfef076dad21cf
ylee <gabrieloboe@gmail.com>
2020-09-24 10:23:32 +0900

    2020 0910A반 thread httpdata 별도 method

--- 03http/src/main/java/com/ylee/a03http/MainActivity.java
+++ 03http/src/main/java/com/ylee/a03http/MainActivity.java
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     String naverurl="https://m.naver.com";
+    String urlStr = "http://api.openweathermap.org/data/2.5/forecast?&APPID=3e0c533ec49a146af626992e6bcce23f&lat=36.1444292&lon=128.391079&mode=xml&units=metric&cnt=15";;
+    String openweatherapi = "http://api.openweathermap.org/data/2.5/weather?&mode=xml&units=metric&appid=3e0c533ec49a146af626992e6bcce23f&q=seoul";
+    String busapi = "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey=vJm8%2BGpTs1b6mG2%2FB%2Ff31iZqhHXyrrnEN8FjnIBUUwaD6fwqu1kY0dDpH4LBNbNVONA43%2Bt88%2BUWxFloMwKv6A%3D%3D&busRouteId=100100414";
+    String airpollutionapi="http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=vJm8%2BGpTs1b6mG2%2FB%2Ff31iZqhHXyrrnEN8FjnIBUUwaD6fwqu1kY0dDpH4LBNbNVONA43%2Bt88%2BUWxFloMwKv6A%3D%3D&numOfRows=20&pageNo=1&sidoName=서울&ver=1.3";
+
     String htmldata = null;
 
     @Override
@@ -43,33 +48,45 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // thread의 문제
+    // 1. 언제 끝날지 모름
+    // 2. 직접 UI에 접근하면 죽음
     class RequestThread extends Thread{
         public void run(){
-            try {
-                URL url = new URL(naverurl);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                if(conn != null){
-                    conn.setConnectTimeout(10000);
-                    conn.setRequestMethod("GET");
-                    conn.setDoInput(true);
-                    conn.setDoOutput(true);
-                    int resCode = conn.getResponseCode();
-                    BufferedReader reader = new BufferedReader
-                            (new InputStreamReader(conn.getInputStream()));
-                    String line;
-                    htmldata = "RR";
-                    while (true){
-                        line = reader.readLine();
-                        if(line == null)
-                            break;
-                        htmldata += line;
-                    }
-                    reader.close();
-                    conn.disconnect();
-                }
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+            htmldata = getHTTPdata(naverurl);
         }
     }
+
+    public String getHTTPdata(String urlStr){
+        String rdata = null;
+
+        try {
+            URL url = new URL(naverurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if(conn != null){
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                int resCode = conn.getResponseCode();
+                BufferedReader reader = new BufferedReader
+                        (new InputStreamReader(conn.getInputStream()));
+                String line;
+                line = reader.readLine();
+                rdata = line;
+                while (true){
+                    line = reader.readLine();
+                    if(line == null)
+                        break;
+                    rdata += line;
+                }
+                reader.close();
+                conn.disconnect();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return rdata;
+    }
+
 }
