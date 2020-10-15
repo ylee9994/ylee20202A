e2804146b1a2f1b1dc4d6306ae538105ddcfdd02
ylee <gabrieloboe@gmail.com>
2020-10-15 12:40:55 +0900

    2020 0910A반

---
+++ 06xmljson/src/main/java/com/ylee/a06xmljson/SBooks.java
@@ -0,0 +1,13 @@
+package com.ylee.a06xmljson;
+
+public class SBooks {
+    String author;
+    String title;
+    Float price;
+
+    public SBooks(String author, String title, Float price) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+    }
+}
